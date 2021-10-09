a626b0cc1eb2e649b020dc453de6b27c5941fb30
Tang Siu Hin <matthew.tang@connect.hku.hk>
2021-10-09 15:18:09 +0800

    try auth success

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
+#!/usr/bin/env python3.9
 # This is a sample Python script.
 
 from ccxt.base.exchange import Exchange
 from dotenv import dotenv_values
+import time
+import hmac
+import urllib.parse
+from requests import Request, Session, Response
+from typing import Tuple
 
-basestring = str
+basestring = str  # ccxt
 
-
-config = dotenv_values(".env")
-
-print(config)
-key = config['API_KEY']
-secret = config['API_SECRET']
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    x = 1
-    y = 2
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+URL = "https://ftx.com/api"
 
 
-# Press the green button in the gutter to run the script.
+def auth(_session: Session, _key: str, _secret: str, _subaccount_name: str = None) -> None:
+    ts = int(time.time() * 1000)
+    request = Request('GET', f'{URL}/account')
+    prepared = request.prepare()
+    signature_payload = f'{ts}{prepared.method}{prepared.path_url}'.encode()
+    signature = hmac.new(_secret.encode(), signature_payload, 'sha256').hexdigest()
+    request.headers['FTX-KEY'] = _key
+    request.headers['FTX-SIGN'] = signature
+    request.headers['FTX-TS'] = str(ts)
+    if _subaccount_name:
+        request.headers['FTX-SUBACCOUNT'] = urllib.parse.quote(_subaccount_name)
+    response = _session.send(request.prepare())
+    data = response.json()
+    print(response.status_code)
+
+
+def process_response(response: Response):
+    try:
+        data = response.json()
+    except ValueError:
+        response.raise_for_status()
+        raise
+    else:
+        if not data['success']:
+            raise Exception(data['error'])
+        return data['result']
+
+
+def get_key() -> Tuple[str, str]:
+    config = dotenv_values(".env")
+    return config['API_KEY'], config['API_SECRET']
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    s = Session()
+    key, secret = get_key()
+    auth(s, key, secret, dotenv_values(".env")['SUBACCOUNT_NAME'])
